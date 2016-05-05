1e939023fb9663d7ab5caf792d0bfe970d666562
janderson99 <james.anderson@bridgevine.com>
2016-05-05 15:20:21 -0400

    NGDC-1465
    NGDC-1465 UX - Prototype Design-Centric-Development Framework

---
+++ js/ajax.js
@@ -0,0 +1,8 @@
+var Ajax = (function() {
+    "use strict";
+
+    // Cross browser Element.matches implementation; see https://goo.gl/3n6CSR
+    var xhr = new XMLHttpRequest();
+    
+    return {}
+}());
