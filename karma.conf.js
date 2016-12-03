dc57fa8ca44e37c0241230e7b34b31bddf89a2fa
janderson99 <james.anderson@bridgevine.com>
2016-12-02 16:22:19 -0800

    NGDC-3431 Normalize "just.js" repo to match other "addless" repos

--- karma.conf.js
+++ karma.conf.js
@@ -1,15 +1,17 @@
 module.exports = function(config) {
     config.set({
         browsers: [
-            'Chrome'/*,
+            'Chrome',
             'Firefox',
             'Safari',
-            'Opera'*/
+            'Opera'
         ],
         files: [
-            'karma.include.js',
-            'src/*.js',
-            'spec/*.js'
+            {pattern: 'node_modules/driven-js/src/**.js', included: true},
+            {pattern: 'karma.include.js', included: true},
+            {pattern: 'src/**.js', included: true},
+            {pattern: 'spec/**.js', included: true},
+            {pattern: 'spec/**.json', included: false}
         ],
         frameworks: [
             'jasmine'
